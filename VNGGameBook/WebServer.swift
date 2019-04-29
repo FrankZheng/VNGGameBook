1a04fe71a77a29c8f79f347b465db6394df4fcac
Frank Zheng <frank.zheng@vungle.com>
2019-04-29 10:07:11 +0800

    1. make /ads work as expected 2. remove unused code

--- VNGGameBook/WebServer.swift
+++ VNGGameBook/WebServer.swift
@@ -37,24 +37,6 @@ class WebServer {
     func start() {
         let webServer = GCDWebServer()
         
-        #if false
-        webServer.addDefaultHandler(forMethod:"GET", request: GCDWebServerRequest.self, processBlock: {request in
-            return GCDWebServerDataResponse(html:"<html><body><p>Hello World</p></body></html>")
-        })
-        
-        #endif
-        
-        #if false
-        webServer.addHandler(forMethod: "GET", path: "/games", request: GCDWebServerRequest.self) { request in
-            let games = [
-                ["title":"", "description":"", "videoURL":"", "postBundle":""],
-                ["title":"", "description":"", "videoURL":"", "postBundle":""],
-                ["title":"", "description":"", "videoURL":"", "postBundle":""],
-            ]
-            return GCDWebServerDataResponse(jsonObject: games)
-        }
-        #endif
-        
         // -> /config
         webServer.addHandler(forMethod: "POST", path: "/config", request: GCDWebServerRequest.self) { request in
             return self.sdkConfigResponse(request)!
@@ -69,7 +51,7 @@ class WebServer {
             return self.okResponse()!
         }
         
-        //static files
+        //host static files
         let appBundlePath = Bundle.main.bundlePath
         webServer.addGETHandler(forBasePath: "/\(staticFolder)/",
                                 directoryPath: appBundlePath,
@@ -77,13 +59,9 @@ class WebServer {
                                 cacheAge: 0,
                                 allowRangeRequests: true)
         
-        
+        //start web server
         webServer.start(withPort:8091, bonjourName: nil)
-        
-        print("Visit \(webServer.serverURL!) in your web browser")
-        
         serverURL = webServer.serverURL
-        
     }
     
     func serverURL(withPath path:String) -> URL? {
@@ -106,7 +84,6 @@ class WebServer {
         guard let configPath = Bundle.main.path(forResource: "config", ofType: "json") else {
             return nil
         }
-        
         guard let configStr = try? String(contentsOf: URL(fileURLWithPath: configPath), encoding: .utf8) else {
             return nil
         }
@@ -119,22 +96,18 @@ class WebServer {
         guard let data = replacedStr.data(using: .utf8) else {
             return nil
         }
-        
         guard var json  = try? JSONSerialization.jsonObject(with:data, options: .mutableContainers) as? [String:AnyObject] else {
             return nil
         }
-        
         guard var placements: [[String:AnyObject]] = json["placements"] as? [[String:AnyObject]] else {
             return nil
         }
-        
         let local: [String:AnyObject] = placements[1]
         for gameItem in gameModel.gameItems {
             var copied = local
             copied["reference_id"] = gameItem.placementId as AnyObject
             placements.append(copied)
         }
-        
         json["placements"] = placements as AnyObject
         return GCDWebServerDataResponse(jsonObject: json)
     }
@@ -144,46 +117,43 @@ class WebServer {
         guard let path = Bundle.main.path(forResource: "ads", ofType: "json") else {
             return nil
         }
-        
         //get the reference id from the request
         guard let json = request.jsonObject as? [String:AnyObject] else {
             return nil
         }
-        
-        guard let req = json["request"] as? [String:AnyObject] else {
+        guard let placements = json["request"]?["placements"] as? [String] else {
             return nil
         }
-        
-        guard let placements = req["placements"] as? [String] else {
-            return nil
-        }
-        
         let placementId = placements[0]
-        
+        //todo: add error handling here
         let gameItem : GameItem = gameModel.gameItems.filter { (item:GameItem) -> Bool in
             return item.placementId == placementId
         }[0]
-        
         let variables = [
             "${placementId}": placementId,
+            "${expiry}": String(getNotExpiredTime()),
             "${videoURL}": staticURL(withPath: gameItem.url.lastPathComponent)?.absoluteString,
             "${postBundle}": staticURL(withPath: gameItem.endcardURL.lastPathComponent)?.absoluteString,
             "${storeURL}": gameItem.storeURL
         ]
-        
         guard let str = try? String(contentsOf: URL(fileURLWithPath: path), encoding: .utf8) else {
             return nil
         }
-        
         let replacedStr = str.stringByReplacingWithVariables(variables)
         let data = replacedStr.data(using: .utf8)
         do {
             let response  = try JSONSerialization.jsonObject(with: data!, options: .allowFragments)
             return GCDWebServerDataResponse(jsonObject: response)
         } catch {
-            print("\(error)")
+            print("Failed to encode json \(error)")
             return nil
         }
     }
     
+    func getNotExpiredTime() -> Int {
+        let daySeconds = TimeInterval(3600.0 * 24)
+        let now = NSDate()
+        let expiredDate = now.addingTimeInterval(daySeconds)
+        return Int(expiredDate.timeIntervalSince1970)
+    }
 }
